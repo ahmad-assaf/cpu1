9f234ae7243396eb0e45841a488f94b5c496e2b7
ahmad-assaf <ahmed.assaf21@gmail.com>
2018-10-06 11:00:41 +0300

    testbench

--- project_4/project_4.srcs/sim_1/new/cpu_Testbench.v
+++ project_4/project_4.srcs/sim_1/new/cpu_Testbench.v
@@ -19,24 +19,26 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-`define REG_SIZE  6'b111111
+`define REG_SIZE  5'b11111
 module cpu_Testbench(
 
     );
     
     reg clk, reset, enable; 
-    wire [`REG_SIZE-1:0] C; 
+    wire [`REG_SIZE-1:0] C=0; 
     wire [`REG_SIZE-1:0] CE=0;
     wire [`REG_SIZE-1:0] CW=0;
     wire [`REG_SIZE-1:0] CN=0;
     wire [`REG_SIZE-1:0] CS=0;
+    
     reg[3:0] x=0,y=0;
+    
     cpu1 thispe(CW,CN,CS,CE,C,clk,enable,x,y);
     
      initial begin
        clk = 0; 
        reset = 0; 
-       enable = 0; 
+       enable = 1; 
      end 
        
      always  
@@ -46,9 +48,12 @@ module cpu_Testbench(
        $display("\t\ttime,\tclk,\treset,\tenable,\tC"); 
      end 
        
-     initial 
-     #10 $finish; 
        
+      always@ (enable==0)
+      begin
+      #1 
+      enable =1;
+      end
      //Rest of testbench code after this line 
      
 endmodule
